d2a141c1c873d14b68ba9e396910bad82451ee8c
Anatole <62328077+a2br@users.noreply.github.com>
2021-02-04 07:24:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# spc-cantine
+
+Simple programme ne comportant qu'une fonction qui va chercher le menu de la cantine et récupérer les infos utiles.
+
+## Example de retour
+
+```json
+{
+	"week": {
+		"from": "01/02/2021",
+		"to": "05/02/2021"
+	},
+	"supplements": "/",
+	"days": [
+		{
+			"name": "LUNDI",
+			"menuItems": [
+				"Charcuterie",
+				"Gnocchi bolognaise",
+				"/",
+				"Poêlée de légumes",
+				"Chanteneige",
+				"Flamby caramel"
+			]
+		},
+		{
+			"name": "MARDI",
+			"menuItems": [
+				"Pâtes au jambon",
+				"Escalope viennoise",
+				"Gratin dauphinois",
+				"Petits pois / carottes",
+				"Emmental stick",
+				"Tentation pâtisserie"
+			]
+		},
+		{
+			"name": "MERCREDI",
+			"menuItems": [
+				"Salade thon / oeuf",
+				"Nuggests de poulet",
+				"Potatoes",
+				"/",
+				"Babybel",
+				"Taillefine"
+			]
+		},
+		{
+			"name": "JEUDI",
+			"menuItems": [
+				"Tomate féta",
+				"Kebab",
+				"Pommes vapeur",
+				"Poêlée de haricots",
+				"Kiri",
+				"Moelleux aux fruits"
+			]
+		},
+		{
+			"name": "VENDREDI",
+			"menuItems": [
+				"Oeuf mimosa",
+				"Dos de cabillaud au",
+				"Riz",
+				"Poêlée de ratatouille",
+				"Vache qui rit",
+				"Danette vanille"
+			]
+		}
+	]
+}
+```
---
+++ index.ts
@@ -0,0 +1,84 @@
+import fetch from "node-fetch";
+import xml from "xml2js";
+
+async function fetchMenu(): Promise<string> {
+	const res = await fetch(
+		"https://www.saintpierrechanel.fr/copie-de-restauration"
+	);
+	const data = await res.text();
+	return data;
+}
+function toJson(html: string): Promise<Record<string, any>> {
+	return xml.parseStringPromise(html, { strict: false });
+}
+
+function processData(
+	data: Record<string, any>
+): {
+	week: { from: string; to: string };
+	supplements: string;
+	days: {
+		name: string;
+		menuItems: string[];
+	}[];
+} {
+	const body = data.HTML.BODY[0];
+	const site = body.DIV[0].DIV[0];
+	const junction = site.DIV.find(
+		(d: any) => d.$.ID === "site-root"
+	).DIV[0].MAIN[0].DIV[0].DIV[0].DIV[0].DIV.find(
+		(d: any) => d.$.CLASS === "_2S9ms"
+	).DIV[0].DIV[0].DIV[0];
+	const cross = junction.SECTION[0].DIV[1].DIV;
+
+	const menu = cross.map((road: Record<string, any>) => {
+		const path = road.DIV.find(
+			(d: Record<string, any>) => d.$["DATA-TESTID"] === "inline-content"
+		).DIV[0];
+		const dayName = path.DIV.find(
+			(d: Record<string, any>) => d.$.ROLE === "button"
+		).DIV[0].SPAN[0]._;
+		const menuItemsRaw = path.DIV.find(
+			(d: Record<string, any>) => d.$["ARIA-LABEL"] === "Matrix gallery"
+		).DIV.find(
+			(d: Record<string, any>) =>
+				d.$["DATA-TESTID"] === "matrix-gallery-items-container"
+		).DIV;
+		const menuItems = menuItemsRaw.map(
+			(item: Record<string, any>) =>
+				item.DIV[0].DIV[0].DIV[0].DIV[0]["WIX-IMAGE"][0].IMG[0].$.ALT
+		);
+		return {
+			name: dayName,
+			menuItems,
+		};
+	});
+
+	const [from, to] = junction.DIV.find((d: any) => d.$.ID === "comp-jpntrto6")
+		.H2[0].SPAN[0].SPAN[0]._.substr("Menu du ".length)
+		.split(" au ");
+	const supplements = junction.DIV.find((d: any) => d.$.ID === "comp-jzwnw9u8")
+		.P[0].SPAN[0].SPAN[0]._.replace(/(\t|\r)/g, "")
+		.replace(/(\n)/g, " ")
+		.substr("Suppléments au lycée : ".length);
+
+	return {
+		week: { from, to },
+		supplements,
+		days: menu,
+	};
+}
+
+export async function getMenu(): Promise<{
+	week: { from: string; to: string };
+	supplements: string;
+	days: {
+		name: string;
+		menuItems: string[];
+	}[];
+}> {
+	const fetched = await fetchMenu();
+	const parsed = await toJson(fetched);
+	const processed = processData(parsed);
+	return processed;
+}
