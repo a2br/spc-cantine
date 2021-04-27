644c643cc908819d97f6cb592244e1aa20992244
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2021-04-27 12:04:26 +0200

    Fixed parsing + changed Prettier printWidth

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import fetch from "node-fetch";
 import xml from "xml2js";
 
+export class ParseError extends Error {
+	constructor(public typeError: TypeError) {
+		super("A parsing error of the menu occured. This most likely means the library is outdated requires an update.");
+	}
+}
+
 async function fetchMenu(): Promise<string> {
-	const res = await fetch(
-		"https://www.saintpierrechanel.fr/copie-de-restauration"
-	);
+	const res = await fetch("https://www.saintpierrechanel.fr/copie-de-restauration");
 	const data = await res.text();
 	return data;
 }
@@ -23,12 +27,9 @@ function processData(
 	}[];
 } {
 	const body = data.HTML.BODY[0];
-	const site = body.DIV[0].DIV[0];
-	const junction = site.DIV.find(
-		(d: any) => d.$.ID === "site-root"
-	).DIV[0].MAIN[0].DIV[0].DIV[0].DIV[0].DIV.find(
-		(d: any) => d.$.CLASS === "_2S9ms"
-	).DIV[0].DIV[0].DIV[0];
+	const site = body.DIV[0] /* id="SITE_CONTAINER" */.DIV[0]; /* id="MAIN_MF" */
+	const junction = site.DIV.find((d: any) => d.$.ID === "site-root").DIV[0].MAIN[0].DIV[0].DIV[0].DIV[0].DIV[1].DIV[0]
+		.DIV[0].DIV[0];
 	const cross: any[] = junction.SECTION[0].DIV[1].DIV;
 
 	const menu = cross
@@ -37,44 +38,31 @@ function processData(
 			name: string;
 			menuItems: string[];
 		} => {
-			const path = road.DIV.find(
-				(d: Record<string, any>) => d.$["DATA-TESTID"] === "inline-content"
-			).DIV[0];
-			const name = path.DIV.find(
-				(d: Record<string, any>) => d.$.ROLE === "button"
-			).DIV[0].SPAN[0]._.trim();
+			const path = road.DIV.find((d: Record<string, any>) => d.$["DATA-TESTID"] === "inline-content").DIV[0];
+			const name = path.DIV.find((d: Record<string, any>) => d.$.ROLE === "button").DIV[0].SPAN[0]._.trim();
 			const menuItemsRaw: any[] = path.DIV.find(
 				(d: Record<string, any>) => d.$["ARIA-LABEL"] === "Matrix gallery"
-			).DIV.find(
-				(d: Record<string, any>) =>
-					d.$["DATA-TESTID"] === "matrix-gallery-items-container"
-			).DIV;
-			const menuItems = menuItemsRaw.map(
-				(item: Record<string, any>): string => {
-					const itemObj = item.DIV[0].DIV[0].DIV[0].DIV[0];
-					// const alt = itemObj["WIX-IMAGE"][0].IMG[0].$.ALT;
-					const text: string = itemObj["DIV"][0].DIV[0].DIV[0]._;
-					const otherLines = itemObj["DIV"][0].DIV[0].P;
-					const line2: string = otherLines ? otherLines[0]._ : "";
-					const final = (line2 ? [text, line2].join(" ") : text).trim();
-					return final;
-				}
-			);
+			).DIV.find((d: Record<string, any>) => d.$["DATA-TESTID"] === "matrix-gallery-items-container").DIV;
+			const menuItems = menuItemsRaw.map((item: Record<string, any>): string => {
+				const itemObj = item.DIV[0].DIV[0].DIV[0].DIV[0];
+				// const alt = itemObj["WIX-IMAGE"][0].IMG[0].$.ALT;
+				const text: string = itemObj["DIV"][0].DIV[0].DIV[0]._;
+				const otherLines = itemObj["DIV"][0].DIV[0].P;
+				const line2: string = otherLines ? otherLines[0]._ : "";
+				const final = (line2 ? [text, line2].join(" ") : text).trim();
+				return final;
+			});
 			return {
 				name,
 				menuItems,
 			};
 		});
 
-	const [from, to]: [string, string] = junction.DIV.find(
-		(d: any) => d.$.ID === "comp-jpntrto6"
-	)
+	const [from, to]: [string, string] = junction.DIV.find((d: any) => d.$.ID === "comp-jpntrto6")
 		.H2[0].SPAN[0].SPAN[0]._.substr("Menu du ".length)
 		.split(" au ")
 		.map((e: string) => e.trim());
-	const supplements: string = junction.DIV.find(
-		(d: any) => d.$.ID === "comp-jzwnw9u8"
-	)
+	const supplements: string = junction.DIV.find((d: any) => d.$.ID === "comp-jzwnw9u8")
 		.P[0].SPAN[0].SPAN[0]._.replace(/(\t|\r)/g, "")
 		.replace(/(\n)/g, " ")
 		.substr("Suppléments au lycée : ".length)
@@ -128,8 +116,13 @@ export interface formattedMenu {
 export async function getMenu(): Promise<formattedMenu> {
 	const fetched = await fetchMenu();
 	const parsed = await toJson(fetched);
-	const processed = processData(parsed);
-	return processed;
+	try {
+		const processed = processData(parsed);
+		return processed;
+	} catch (err) {
+		if (err instanceof TypeError) throw new ParseError(err);
+		throw err;
+	}
 }
 
 export default getMenu;
